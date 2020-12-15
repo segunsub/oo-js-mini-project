7ef464446162329e9de8b018480074595e319a34
ShemarG <shemar.gordon32@gmail.com>
2020-12-15 19:10:19 +0000

    added drive and charge

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ class Tesla {
   applyTrim (trim) {
     
   }
+  drive (distance) {
+    if (distance > this.rangeRemaining) {
+      return 'You do not have enough charge to make it that far!'
+    }
+    this.odometer += distance
+    this.rangeRemaining -= distance
+    return `You have successfully driven ${distance} miles, you have ${this.rangeRemaining} miles left.`
+  }
+  charge (time, superCharge) {
+    if (superCharge) {
+      let rangeAdded = Math.floor(time / 30)*200 + (time % 30)*(200/30)
+      let chargeBill = (time*0.03).toFixed(2)
+      if (this.rangeRemaining + rangeAdded > this.maxRange) {
+        this.rangeRemaining = this.maxRange
+        return `Your Tesla is fully charged! Your account will be charged $${chargeBill}.`
+      } else {
+        this.rangeRemaining += rangeAdded
+        return `Your Tesla has been charged, it now has a range of ${this.rangeRemaining} miles. Your account will be charged $${chargeBill}.`
+      }
+    } else {
+      let rangeAdded = Math.floor(time / 30)*50 + (time % 30)*(50/30)
+      if (this.rangeRemaining + rangeAdded > this.maxRange) {
+        this.rangeRemaining = this.maxRange
+        return `Your Tesla is fully charged!`
+      } else {
+        this.rangeRemaining += rangeAdded
+        return `Your Tesla has been charged, it now has a range of ${this.rangeRemaining} miles.`
+      }
+    }
+  }
 }
 
 
