1c35509a68407987460fe2166d373adef6536017
ShemarG <shemar.gordon32@gmail.com>
2020-12-15 16:17:31 +0000

    skeleton

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Write your classes below this line
+class Tesla {
+  
+}
 
+class Person {
+  
+}
 
-
-
-
+class Trip {
+  
+}
 
 // Write your data (instances and method calls) below this line
 console.log("Welcome to <<app name here>>!")
